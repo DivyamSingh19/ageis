0df916dd126722bb32140bde0629207e2344bbc5
Divyam Singh <divyamsingh1918@gmail.com>
2026-02-20 12:56:09 +0530

    updates

---
+++ apps/api/index.ts
@@ -0,0 +1,24 @@
+import express,{Request,Response} from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+const app = express()
+const port = 4000
+dotenv.config()
+app.use(express.json())
+app.get("/",async (req:Request,res:Response) => {
+    try {
+        res.json({
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({
+            message:"API failed"
+        })
+    }
+})
+
+app.listen(port,()=>{
+    console.log("Server started on :",port);
+    
+})
