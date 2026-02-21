4f740dd3484bcdb1936bd233d9a6e9ed581829b2
Divyam Singh <divyamsingh1918@gmail.com>
2026-02-21 14:31:08 +0530

    updates

--- apps/api/controllers/farmer/product.controller.ts
+++ apps/api/controllers/farmer/product.controller.ts
@@ -1,3 +1,31 @@
+import { Request,Response } from "express"
 export class ProductController {
-    
+  create = async (req:Request,res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+  }   
+  edit = async (req:Request,res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+  }
+  all = async (req:Request,res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+  }
+  id = async (req:Request,res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+  }
 }
