fd0341df9f9b95bb873c81d99299b3efaf2d2178
Divyam Singh <divyamsingh1918@gmail.com>
2026-02-21 09:50:56 +0530

    updates

---
+++ apps/api/routes/user/auth.route.ts
@@ -0,0 +1,13 @@
+import { AuthController } from "../../controllers/user/auth.controller";
+import { Router } from "express";
+
+
+
+const router = Router()
+const authController = new AuthController()
+
+router.post("/register",authController.register)
+router.post("/login",authController.login)
+router.get("/me",authController.me)
+
+export default router
