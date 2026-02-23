b1eeaea150e136252a23cc87d2dd66bdde6a62a2
Divyam Singh <divyamsingh1918@gmail.com>
2026-02-24 02:56:25 +0530

    updates

---
+++ apps/api/blockchain/controllers/key.controller.ts
@@ -0,0 +1,11 @@
+import prisma from "@repo/db";
+import { Request,Response } from "express";
+
+
+
+export class KeyController {
+    createKeyPair = async (req:Request,res:Response) => {
+        
+    }
+
+}
---
+++ main/app/(tabs)/marketplace.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const marketplace = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default marketplace
