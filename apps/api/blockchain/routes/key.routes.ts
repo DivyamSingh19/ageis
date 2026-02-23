235b78d2cd69184e6893dd8de14c9474d3c98cee
Divyam Singh <divyamsingh1918@gmail.com>
2026-02-24 03:28:52 +0530

    updates

--- apps/api/blockchain/routes/key.routes.ts
+++ apps/api/blockchain/routes/key.routes.ts
@@ -1,1 +1,21 @@
 import { Router } from "express";
+import { KeyController } from "../controllers/key.controller";
+
+const router = Router();
+const keyController = new KeyController();
+
+ 
+router.post("/new", keyController.createKeyPair);
+
+ 
+router.get("/:publicKey", keyController.getKeys);
+
+ 
+router.get("/:publicKey/public", keyController.getPublicKey);
+
+ 
+router.put("/:publicKey/rotate", keyController.rotateKeys);
+
+router.delete("/:publicKey", keyController.deleteKeys);
+
+export default router;
