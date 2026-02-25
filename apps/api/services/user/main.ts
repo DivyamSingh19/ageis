c6a069f5bd8727e8131a8a633a7aa223581ea255
Divyam Singh <divyamsingh1918@gmail.com>
2026-02-25 23:51:28 +0530

    updates

--- apps/api/services/user/main.ts
+++ apps/api/services/user/main.ts
@@ -1,12 +1,25 @@
 import prisma from "../../db/prisma";
 
-
 export class UserService{
     async findUser(email:string){
         try {
-            
+            const user = await prisma.user.findUnique({
+                where:{email}
+            })
+            return user;
         } catch (error) {
-            
+            throw error;
         }
     }
+    async UserById(id:string){
+        try {
+            const user = await prisma.user.findUnique({
+                where:{id}
+            })
+            return user;
+        } catch (error) {
+            throw error;
+        }
+    }
+    
 }
