68b485bc211bc2b4c52552e7fda0d05d0ece1c01
Divyam Singh <divyamsingh1918@gmail.com>
2026-02-28 09:15:38 +0530

    updates

--- api/routes/farmer/product.ts
+++ api/routes/farmer/product.ts
@@ -5,14 +5,14 @@ import { farmerAuth } from "../../middlewares/farmer";
 
 const productRouter = Router();
 const controller = new ProductController();
+ 
+productRouter.get("/discover", controller.discover);
+productRouter.get("/all", farmerAuth, controller.all);
 
 productRouter.post("/", farmerAuth, upload.array("files", 5), controller.create);
 productRouter.put("/:id", farmerAuth, upload.array("files", 5), controller.update);
 productRouter.delete("/:id", farmerAuth, controller.delete);
 productRouter.get("/:id/nft", controller.getNftInfo);
 productRouter.get("/:id", controller.getById);
-productRouter.get("/", farmerAuth, controller.all);
-productRouter.get("/all", controller.all);
-productRouter.get("/discover", controller.discover);
 
 export default productRouter;
